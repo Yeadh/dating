a4cedffd3d1da6dcd7e5856da0cb991ea65c23fa
tanvirr99 <tanberr90@gmail.com>
2019-10-17 17:38:59 +0600

    initial
    home er responsive MD device pojontto complete

---
+++ js/main.js
@@ -0,0 +1,222 @@
+(function ($) {
+"use strict";
+
+// preloader
+function preloader() {
+	$('#preloader').delay(0).fadeOut();
+};
+
+$(window).on('load', function () {
+	preloader(),
+		wowanimation();
+});
+
+
+// meanmenu
+$('#mobile-menu').meanmenu({
+	meanMenuContainer: '.mobile-menu',
+	meanScreenWidth: "992"
+});
+
+
+// sticky
+$(window).on('scroll', function () {
+	var scroll = $(window).scrollTop();
+	if (scroll < 245) {
+		$(".header-sticky").removeClass("sticky");
+	} else {
+		$(".header-sticky").addClass("sticky");
+	}
+});
+
+
+
+// mainSlider
+function mainSlider() {
+	var BasicSlider = $('.slider-active');
+	BasicSlider.on('init', function (e, slick) {
+		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+		doAnimations($firstAnimatingElements);
+	});
+	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+		doAnimations($animatingElements);
+	});
+	BasicSlider.slick({
+		autoplay: false,
+		autoplaySpeed: 10000,
+		dots: false,
+		fade: true,
+		arrows: false,
+		responsive: [
+			{ breakpoint: 767, settings: { dots: false, arrows: false } }
+		]
+	});
+
+	function doAnimations(elements) {
+		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		elements.each(function () {
+			var $this = $(this);
+			var $animationDelay = $this.data('delay');
+			var $animationType = 'animated ' + $this.data('animation');
+			$this.css({
+				'animation-delay': $animationDelay,
+				'-webkit-animation-delay': $animationDelay
+			});
+			$this.addClass($animationType).one(animationEndEvents, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+}
+mainSlider();
+
+
+// testimonial-active
+$('.testimonial-nav-active').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	asNavFor: '.testimonial-active',
+	dots: false,
+	arrows: false,
+	speed: 1000,
+	autoplay: true,
+	centerMode: true,
+	centerPadding: '0px',
+	focusOnSelect: true,
+	responsive: [
+		{
+		breakpoint: 1200,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		},
+		{
+		breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			}
+		},
+		{
+		breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				arrows: false,
+			}
+		},
+		{
+		breakpoint: 575,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+			}
+		},
+	]
+});
+$('.testimonial-active').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	autoplay: true,
+	speed: 1000,
+	asNavFor: '.testimonial-nav-active',
+	responsive: [
+		{
+		breakpoint: 1200,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
+		breakpoint: 767,
+			settings: {
+				fade: true,
+				arrows: false,
+			}
+		},
+	]
+});
+
+
+// niceSelect;
+$(".selected").niceSelect();
+
+
+/* magnificPopup img view */
+$('.popup-image').magnificPopup({
+	type: 'image',
+	gallery: {
+	  enabled: true
+	}
+});
+
+/* magnificPopup video view */
+$('.popup-video').magnificPopup({
+	type: 'iframe'
+});
+
+
+// isotop
+$('.grid').imagesLoaded( function() {
+	// init Isotope
+	var $grid = $('.grid').isotope({
+	  itemSelector: '.grid-item',
+	  percentPosition: true,
+	  masonry: {
+		columnWidth: '.grid-item',
+	  }
+	});
+	// filter items on button click
+	$('.portfolio-menu').on( 'click', 'button', function() {
+		var filterValue = $(this).attr('data-filter');
+		$grid.isotope({ filter: filterValue });
+	});
+});
+
+//for menu active class
+$('.portfolio-menu button').on('click', function(event) {
+	$(this).siblings('.active').removeClass('active');
+	$(this).addClass('active');
+	event.preventDefault();
+});
+
+
+// aos-active
+AOS.init({
+	duration: 1000,
+	mirror: true
+});
+
+
+
+// scrollToTop
+$.scrollUp({
+	scrollName: 'scrollUp',
+	topDistance: '300',
+	topSpeed: 300,
+	animation: 'fade',
+	animationInSpeed: 200,
+	animationOutSpeed: 200,
+	scrollText: '<i class="fas fa-chevron-up"></i>',
+	activeOverlay: false,
+});
+
+// WOW active
+function wowanimation() {
+	var wow = new WOW({
+		boxClass: 'wow',
+		animateClass: 'animated',
+		offset: 0,
+		mobile: false,
+		live: true
+	});
+	wow.init();
+}
+
+
+})(jQuery);
